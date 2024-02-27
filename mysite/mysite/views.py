b5a9741c5b25d54a009dab3095f4ad2da9e1b4b0
RNG21 <frankytsang921@gmail.com>
2024-02-27 14:18:20 +0000

    Add views.py

---
+++ mysite/mysite/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Can add more views here. I think these are all the ones the navbar links to ? 
+def home(request):
+    return render(request, 'home.html')
+def base(request):
+    return render(request, 'mysite/base.html')
+def settings(request):
+    return render(request, 'settings.html')
+def leaderboard(request):
+    return render(request, 'leaderboard.html')
+def challenges(request):
+    return render(request, 'challenges.html')
+def login(request):
+    return render(request, 'login.html')
