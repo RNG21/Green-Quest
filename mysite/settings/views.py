a803afd1523c415e7155efe9c9edf1febcae738e
Talon-Wu <148297662+Talon-Wu@users.noreply.github.com>
2024-02-29 06:23:13 +0000

    setting-enhanced

--- mysite/settings/views.py
+++ mysite/settings/views.py
@@ -7,8 +7,11 @@ from django.contrib.auth.models import User
 
 @login_required(login_url='/login')
 def render_settings(request):
-    return render(request, 'settings.html')
+    return render(request, 'settings/base.html')
 
+# @login_required(login_url='/login')
+# def render_settings2(request):
+#     return render(request, 'settings/base.html')
 
 @login_required(login_url='/login')
 def delete_account(request):
