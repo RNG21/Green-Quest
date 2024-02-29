ffa6f35cbb366c84e30244d67cdb863f634cb4b3
RNG21 <frankytsang921@gmail.com>
2024-02-29 05:02:52 +0000

    feat: add challenges when there's none

--- mysite/challenges/views.py
+++ mysite/challenges/views.py
@@ -5,7 +5,21 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.conf import settings
 
-from db.models import Task
+from leaderboard.models import LeaderboardEntry
+from db.models import Task, User, TaskType, Location
+
+def completed_challenge(request: HttpRequest) -> None:
+    username = request.POST["username"]
+    score = request.POST["score"]
+
+    user = User.objects.get(username=username)
+    LeaderboardEntry(user=user, score=score).save()
+
+def add_challenge(request: HttpRequest) -> None:
+    task_name, task_des, location_name = request.POST["task_name"], request.POST["task_des"], request.POST["location"]
+    
+    location = Location.objects.get(name=location_name)
+    TaskType(task_name, task_des, location).save()
 
 
 def render_map(request: HttpRequest, tasks: Iterable[Task]=...):
@@ -20,7 +34,8 @@ def render_map(request: HttpRequest, tasks: Iterable[Task]=...):
         "menu_items": tasks,
         "API_KEY": settings.MAPS_API_KEY,
         "positions": json.dumps(positions),
-        "center": json.dumps(settings.MAPS_CENTER_COORDINATES)
+        "center": json.dumps(settings.MAPS_CENTER_COORDINATES),
+        "is_admin": True
     }
     return render(request, "challenges/map.html", context=context)
 
