ef46a077564b7899f21bfb9ff7f8930d858a0416
Gcooley512 <gec217@exeter.ac.uk>
2024-03-19 12:58:46 +0000

    Delete UserTasks when deleting account

--- mysite/settings/views.py
+++ mysite/settings/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from db.models import UserTask
 
 
 @login_required(login_url='/login')
@@ -18,9 +19,8 @@ def delete_account(request):
     if request.method == 'POST':
         password = request.POST.get('password')
         if request.user.check_password(password):
-            # TODO: delete user tasks
-
-
+            # delete user tasks
+            UserTask.objects.filter(user=request.user).delete()
             # delete account.
             request.user.delete()
             # logout the user
