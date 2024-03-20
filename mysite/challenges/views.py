7bcf2f04646004f33748fa337d4b73d0b60b8afd
RNG21 <frankytsang921@gmail.com>
2024-03-20 21:33:30 +0000

    fix: add check on loading tasks sidebar

--- mysite/challenges/views.py
+++ mysite/challenges/views.py
@@ -6,13 +6,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.conf import settings
 
-from db.models import Task, User, TaskType, Location, CompleteTask
-
-def task_view(request, task_id):
-    # method used to send task data to html
-    task = Task.objects.get(id=task_id)
-    context = {'task': task}
-    return render(request, 'myapp/Task_chinese.html', context)
+from db.models import Task, Location, CompleteTask
 
 
 def completed_challenge(request: HttpRequest) -> None:
@@ -35,19 +29,23 @@ def render_map(request: HttpRequest, tasks: Iterable[Task]=Task.objects.all()):
     if request.method == "POST":
         completed_challenge(request)
 
+    # Organising data into formats for front-end rendering
     loc_tasks = {}
     positions = []
     for task in tasks:
+        # Tasks with no location
         if task.location is None:
             task.location = Location()
             task.location.locationName = "Extras"
             task.location.longtitude = None
             task.location.latitude = None
 
+        # Group tasks by their location, data for sidebar showing active tasks
         if str(task.location) not in loc_tasks:
             loc_tasks[str(task.location)] = []
         loc_tasks[str(task.location)].append(task)
 
+        # Data for rendering map in js
         positions.append(
             {
                 "lat": task.location.latitude,
@@ -55,7 +53,10 @@ def render_map(request: HttpRequest, tasks: Iterable[Task]=Task.objects.all()):
                 "location_name": str(task.location)
             }
         )
-    loc_tasks["Extras"] = loc_tasks.pop("Extras")
+
+    # Put extras (tasks with no location) at the bottom of all the locations
+    if loc_tasks.get("Extras"):
+        loc_tasks["Extras"] = loc_tasks.pop("Extras")
 
     context = {
         "API_KEY": settings.MAPS_API_KEY,
