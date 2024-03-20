6d1fd60e72cd1031f9c6600334d8b5726b776381
RNG21 <frankytsang921@gmail.com>
2024-03-20 17:21:39 +0000

    refactor: allow blank location in task

--- mysite/db/models.py
+++ mysite/db/models.py
@@ -7,8 +7,8 @@ from django.db.models import Count
 class Location(models.Model):
     locationName = models.CharField(max_length=100)
     description = models.CharField(max_length=100, default='Default description')
-    longtitude = models.FloatField(default=1)
-    latitude = models.FloatField(default=1)
+    longtitude = models.FloatField(null=True)
+    latitude = models.FloatField(null=True)
     def __str__(self):
         return f" {self.locationName}"
 
@@ -22,7 +22,7 @@ class Task(models.Model):
     title = models.CharField(max_length=255, default='Untitled Task')
     taskType = models.ForeignKey(TaskType, on_delete=models.CASCADE, null=True)
     description = models.TextField(default='Default description')
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self):
         return f'{self.location.locationName} - {self.title}'
 
