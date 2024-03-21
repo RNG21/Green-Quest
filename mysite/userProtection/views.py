48ca5fa288dc8097b9636a220aa2786e62eb6a0f
YSREI <1438250qwer@gmail.com>
2024-03-21 09:31:49 +0000

    Modify text in user protection

--- mysite/userProtection/views.py
+++ mysite/userProtection/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 
 # Create your views here.
 def render_userProtection(request):
-    return render(request, 'userProtection.html')
+    admin = User.objects.filter(is_staff=True).first()
+    return render(request, 'userProtection.html',context={"admin_email": admin.email})
